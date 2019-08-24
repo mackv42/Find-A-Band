4122d2dc4b6c0d70a651157caa300b081fb54d4a
Vincent Mackay <mackv42@gmail.com>
2019-08-23 19:26:46 -0500

    added list controller for genres

--- FindABand/Controllers/GenreController.cs
+++ FindABand/Controllers/GenreController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FindABand.Data;
+using FindABand.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,85 +11,17 @@ namespace FindABand.Controllers
 {
     public class GenreController : Controller
     {
-        // GET: Genre
-        public ActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public GenreController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        // GET: Genre/Details/5
-        public ActionResult Details(int id)
+        [HttpGet]
+        public List<Genre> List()
         {
-            return View();
-        }
-
-        // GET: Genre/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Genre/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Genre/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Genre/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Genre/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Genre/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return _context.Genres.Where(x => true).ToList();
         }
     }
 }
