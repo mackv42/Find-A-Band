17b656667745c3a79bbdc5631a0da497a4fa474d
Vincent Mackay <mackv42@gmail.com>
2019-08-23 19:39:03 -0500

    Added jquery for list of genres

--- FindABand/wwwroot/js/Genres.js
+++ FindABand/wwwroot/js/Genres.js
@@ -1,1 +1,28 @@
-﻿
+﻿genreList = [];
+function getGenreList() {
+    $.ajax({
+        type: "GET",
+        url: "/Genre/List",
+        success(data) {
+            //jquery to populate select list
+            console.log(data);
+            genreList = data;
+            var genres = document.getElementsByClassName("genres");
+           
+            for (var j = 0; j < genres.length; j++) {
+                for (var i = 0; i < data.length; i++) {
+                    //genreList.push({ id: data[i].Id, name: data[i].categoryName });
+                    
+                    genres[j].innerHTML += "<option value=" + data[i].genreId + ">" + data[i].name + "</option>"
+                }
+            }
+        }
+    });
+}
+getGenreList();
+function getGenre(id) {
+    //console.log(genreList + "genres here");
+
+    var arr = genreList.filter(x => x.id == id);
+    return arr[0].categoryName;
+}
