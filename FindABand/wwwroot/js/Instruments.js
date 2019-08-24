d21e11ccb7c3e0a729ce73d92a9283379eef86f1
Vincent Mackay <mackv42@gmail.com>
2019-08-23 19:43:06 -0500

    Added jquery for list of instruments

--- FindABand/wwwroot/js/Instruments.js
+++ FindABand/wwwroot/js/Instruments.js
@@ -1,1 +1,28 @@
-﻿
+﻿instrumentList = [];
+function getInstrumentList() {
+    $.ajax({
+        type: "GET",
+        url: "/Instrument/List",
+        success(data) {
+            //jquery to populate select list
+            console.log(data);
+            instrumentList = data;
+            var instruments = document.getElementsByClassName("instruments");
+
+            for (var j = 0; j < instruments.length; j++) {
+                for (var i = 0; i < data.length; i++) {
+                    //genreList.push({ id: data[i].Id, name: data[i].categoryName });
+
+                    instruments[j].innerHTML += "<option value=" + data[i].instrumentId + ">" + data[i].name + "</option>"
+                }
+            }
+        }
+    });
+}
+getInstrumentList();
+function getInstrument(id) {
+    //console.log(genreList + "genres here");
+
+    var arr = instrumentList.filter(x => x.id == id);
+    return arr[0].name;
+}
