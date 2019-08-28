3a4de9cd2155885e5711ca5d2e189a13849e4925
Vincent Mackay <mackv42@gmail.com>
2019-08-28 14:01:18 -0500

    removed unnecessary scaffolded methods in message controller

--- FindABand/Controllers/MessageController.cs
+++ FindABand/Controllers/MessageController.cs
@@ -19,10 +19,7 @@ namespace FindABand.Controllers
         }
 
             // GET: Message
-        public ActionResult Index()
-        {
-            return View();
-        }
+      
         public ActionResult Messages(int id)
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -33,79 +30,6 @@ namespace FindABand.Controllers
 
             return View(messages);
         }
-        // GET: Message/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Message/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Message/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Message/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Message/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Message/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Message/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+      
     }
 }
