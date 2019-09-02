67e43e8e9126607a08d1d11d3f142d84a0b67d5a
Vincent Mackay <mackv42@gmail.com>
2019-09-02 16:45:29 -0500

    band Roadie test complete

---
+++ FindABand/Controllers/BandRoadieTestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FindABand.Data;
+using FindABand.Models;
+using FindABand.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FindABand.Controllers
+{
+    public class BandRoadieTestController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BandRoadieTestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Create(int bandId)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userAccount = _context.UserAccounts.Where(x => x.UserId == userId).FirstOrDefault();
+            var band = _context.Bands.Where(x => x.BandId == bandId).FirstOrDefault();
+
+            BandRoadieTestQuestionViewModel model = new BandRoadieTestQuestionViewModel();
+            model.NewQuestion = new BandRoadieTestQuestion();
+            model.NewQuestion.BandId = bandId;
+            model.TestQuestions = _context.BandRoadieTestQuestions.Where(x => x.BandId == bandId).ToList();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(BandRoadieTestQuestion NewQuestion)
+        {
+            var addQuestion = new BandRoadieTestQuestion();
+            addQuestion.BandId = NewQuestion.BandId;
+            addQuestion.Question = NewQuestion.Question;
+            addQuestion.Weight = NewQuestion.Weight;
+            _context.BandRoadieTestQuestions.Add(addQuestion);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Create", new { bandId = addQuestion.BandId });
+        }
+    }
+}
---
+++ FindABand/ViewModels/BandRoadieTestQuestionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using FindABand.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindABand.ViewModels
+{
+    public class BandRoadieTestQuestionViewModel
+    {
+        public BandRoadieTestQuestionViewModel()
+        {
+
+        }
+
+        public List<BandRoadieTestQuestion> TestQuestions { get; set; }
+        public BandRoadieTestQuestion NewQuestion { get; set; }
+    }
+}
